ba4965c2b4226355a6695a4525a5a7910691fbdb
rahulshet <rahulushet@gmail.com>
2026-07-26 03:48:48 +0530

    loop

--- loop.java
+++ loop.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class loop {
     public static void main(String[] args) {
-        
+        //printing 10 multiple of 2
+        // for(int i=1;i<=10;i++){
+        //     System.out.println("2 * "+i+" = "+2*i);
+        // }
+        //printing prime number
+        boolean isprime = true;
+        Scanner sc = new Scanner(System.in);
+        int num  = sc.nextInt();
+        if(num <=1){
+            isprime = false;
+        }
+        for(int i = 2; i<num; i++){
+            if(num%i == 0){
+                isprime = false;
+                System.out.println(isprime);
+                break;
+            }
+        }
+        sc.close();
     }
+    
 }
