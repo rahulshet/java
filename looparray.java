83db27cdbc3cc82aa8230a96bce081b4974024c7
rahulshet <rahulushet@gmail.com>
2026-07-26 20:02:40 +0530

    loops and array

---
+++ looparray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class looparray {
+    
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        //insert n number in array and print those
+        // System.out.println("Enter length of array: ");
+        // int num = sc.nextInt();
+        // int[] arr = new int[num];
+        // System.out.println("Enter the numbers: ");
+        // for(int i = 0; i<num;i++){
+        //     arr[i] = sc.nextInt();
+        // }
+        // System.out.println("numbers are :");
+        // for(int j = num-1; j >= 0; j--){
+        //     System.out.println(arr[j]);
+        // }
+         //fid sum of array
+
+         int[] arr = {2,4,3,5,9};
+         
+
+
+
+       sc.close();
+    }
+}
---
+++ problems/prblm1.java
@@ -0,0 +1,7 @@
+package problems;
+
+public class prblm1 {
+    public static void main(String[] args) {
+        //
+    }
+}
