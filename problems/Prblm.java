cf6d2f9261dd9c1aa15814acb1c2c97d5cabf35d
rahulshet <rahulushet@gmail.com>
2026-08-03 23:02:49 +0530

    ggg

---
+++ problems/Prblm.java
@@ -0,0 +1,42 @@
+package problems;
+
+public class Prblm{
+    public static void main(String[] args) {
+        //pattern problems
+        // int num = 5;
+        // for(int i= 0; i<num; i++){
+        //     for(int j =0;j<num;j++){
+        //         System.out.print("*");
+        //     }
+        //        System.out.println();
+        // }
+
+        //right angle triangle pattern *
+        // int num = 5;
+        // for (int i = 0; i<num; i++){
+        //     for (int j= 0; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        
+        //  int num = 5;
+        // for (int i = 0; i<num; i++){
+        //     for (int j= 0; j<=i; j++){
+                
+        //         System.out.print(i+1);
+        //     }
+        //     System.out.println();
+        // }
+        // int n = 5;
+        // for(int i=n; i>0; i--){
+        //     for (int j=0; j<i; j++){
+        //         System.out.print(j+1);
+        //     }
+        //     System.out.println();
+        // }
+        // equal triangle
+        
+        
+    }
+}
--- problems/prblm1.java
+++
@@ -1,7 +0,0 @@
-package problems;
-
-public class prblm1 {
-    public static void main(String[] args) {
-        //
-    }
-}
